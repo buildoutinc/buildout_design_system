f1e251d46b1161e6b59954de83aac01d3b489877
Joel <joel.lopez@buildout.com>
2023-12-20 14:38:19 -0800

    moving package json to install directory

--- lib/generators/buildout_design_system/install/install_generator.rb
+++ lib/generators/buildout_design_system/install/install_generator.rb
@@ -1,7 +1,7 @@
 module BuildoutDesignSystem
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('../../../..', __FILE__);
+      source_root File.expand_path('.', __FILE__);
 
       def merge_package_json
         destination = 'package.json'
@@ -28,12 +28,12 @@ module BuildoutDesignSystem
 
       def append_to_procfile
         if File.exist?('Procfile.dev')
-          append_to_file 'Procfile.dev', 'yarn watch:css'
+          append_to_file 'Procfile.dev', 'css: yarn watch:css'
         else
           say 'Procfile.dev not found, skipping yarn watch:css', :yellow
         end
-        if File.exist('Procfile')
-          append_to_file 'Procfile', 'yarn build:css'
+        if File.exist?('Procfile')
+          append_to_file 'Procfile', 'css: yarn build:css'
         else
           say 'Procfile not found, skipping yarn build:css', :yellow
         end
