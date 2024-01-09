99d8526e1fd9348d88be016d934de0c73c99fd48
Joel <joel.lopez@buildout.com>
2024-01-08 16:04:34 -0800

    starting to move the css over to the new home.

--- app/components/buildout_design_system/badge.rb
+++ app/components/buildout_design_system/badge.rb
@@ -2,14 +2,25 @@
 
 module BuildoutDesignSystem
   class Badge < ViewComponent::Base
-    def initialize( # rubocop:disable Lint/MissingSuper
-      variant: "primary",
-      icon: nil,
-      size: nil
-    )
-      @variant = ::BuildoutDesignSystem::Constants::THEMES.fetch(variant.to_sym)
-      @icon = icon
-      @size_class = size ? "badge-#{size}" : size
-    end
+    VARIANTS = {
+        primary: "primary",
+        secondary: "secondary",
+        success: "success",
+        danger: "danger",
+        warning: "warning",
+        info: "info",
+        neutral: "neutral",
+      }.freeze
+
+      DEFAULT_VARIANT = VARIANTS.fetch(:primary)
+
+      def initialize(options = {}, **attrs)
+        super(**attrs)
+        @variant = VARIANTS.fetch(options[:variant]&.to_sym || :primary, DEFAULT_VARIANT)
+        @icon = options[:icon]
+        @class_name = options[:class_name]
+        @placement = options.fetch(:placement, "start")
+        @attrs = attrs
+      end
   end
 end
