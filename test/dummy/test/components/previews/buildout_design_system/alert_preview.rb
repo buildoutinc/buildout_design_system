f33a176bbeee7f48029e801df8674e585e595eb6
Joel <joel.lopez@buildout.com>
2024-01-09 12:08:39 -0800

    Adding Alert Component

---
+++ test/dummy/test/components/previews/buildout_design_system/alert_preview.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+module BuildoutDesignSystem
+  class AlertPreview < ViewComponent::Preview
+    # @!group Banner/Alert
+
+    # Banner / Alert
+    # -------------------------
+    # Banners are used to display important information to the user.They can use various
+    # colors and attributes to convey the severity of the message. They can also become
+    # dismissable.
+    #
+    # @param status select "The status of the banner" { choices: ["info", "success", "error", "warning"] }
+    # @param icon "Icon Name from FontAwesome including the fa- prefix"
+    # @param title "Title of the banner"
+    # @param content "Message of the banner"
+    # @param class_name "Additional Classes to add to the banner"
+    # @param dismissable toggle "Boolean to determine if the banner is dismissable"
+    def default(  # rubocop:disable Metrics/ParameterLists
+      status: "info",
+      icon: "fa-info-circle",
+      title: "Alert Title",
+      class_name: "",
+      dismissable: false,
+      content: "This is the banner message"
+    )
+      render(Alert.new(
+              status: status,
+              icon: icon,
+              title: title,
+              class_name: class_name,
+              dismissable: dismissable
+            )) do
+        content
+      end
+    end
+    # @!endgroup
+  end
+end
