49e42fddd1191e7f28a8c632f8c73ce2b61fe89a
Joel <joel.lopez@buildout.com>
2024-01-11 13:46:19 -0800

    adding status icon to snackbar notifications

--- app/components/buildout_design_system/snackbar.rb
+++ app/components/buildout_design_system/snackbar.rb
@@ -4,17 +4,35 @@ module BuildoutDesignSystem
   class Snackbar < ViewComponent::Base
     renders_one :action, "SnackBarActionComponent"
 
-      def initialize(title:, message:, **attrs)
-        super(**attrs)
-        @title = title
-        @message = message
-        @attrs = attrs
-      end
+    STATUS = {
+      default: nil,
+      success: "success",
+      warning: "warning",
+      danger: "danger",
+      info: "info"
+    }.freeze
 
-      class SnackBarActionComponent < ViewComponent::Base
-        def call
-          content
-        end
+    ICON = {
+      default: nil,
+      success: "fa-circle-check",
+      warning: "fa-exclamation-triangle",
+      danger: "fa-exclamation-circle",
+      info: "fa-info-circle"
+    }
+
+    def initialize(title:, message:, status: STATUS[:default], **attrs)
+      super(**attrs)
+      @title = title
+      @message = message
+      @status = STATUS.fetch(status&.to_sym, STATUS[:default])
+      @status_icon = ICON.fetch(status&.to_sym, ICON[:default])
+      @attrs = attrs
+    end
+
+    class SnackBarActionComponent < ViewComponent::Base
+      def call
+        content
       end
+    end
   end
 end
