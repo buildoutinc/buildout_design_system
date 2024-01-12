a12b2ae9bf7eb26a43824b0d5e6d8b378a8b08ed
Joel <joel.lopez@buildout.com>
2024-01-12 12:06:01 -0800

    fixing toggle css. adding big toggle css.

--- app/components/buildout_design_system/toggle.rb
+++ app/components/buildout_design_system/toggle.rb
@@ -3,10 +3,17 @@ module BuildoutDesignSystem
   class Toggle < ViewComponent::Base
     attr_reader :class_name, :label, :id, :checked, :name, :value, :disabled, :data
 
-    def initialize(class_name: "", checked: false, value: nil, disabled: false, **attrs)
+    VARIANT = {
+      simple: "simple",
+      verbose: "verbose"
+    }
+
+
+    def initialize(class_name: "", checked: false, value: nil, disabled: false, variant: VARIANT[:simple], **attrs)
       super(**attrs)
       @class_name = class_name
       @checked = checked
+      @variant = VARIANT[variant.to_sym] || VARIANT[:simple]
       @name = name
       @value = value
       @disabled = disabled
