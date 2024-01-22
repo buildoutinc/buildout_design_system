00a2c3427708cd9bb132756c12f425616b78735d
tjacobs3 <tj@buildout.com>
2024-01-22 11:19:49 -0600

    Add helpers for rendering within host app
    Sample: component('Button', { variant: "light" }) { "Button Text" }

--- app/helpers/buildout_design_system/application_helper.rb
+++ app/helpers/buildout_design_system/application_helper.rb
@@ -1,4 +1,18 @@
 module BuildoutDesignSystem
   module ApplicationHelper
+    def component(component_name, *args, **kwargs, &block)
+      render(infer_class(component_name).new(*args, **kwargs), &block)
+    end
+
+    def infer_class(class_name)
+      @@class_map ||= {}
+      return @@class_map[class_name] if @@class_map[class_name].present?
+
+      klass = "::BuildoutDesignSystem::#{class_name}".constantize
+      @@class_map[class_name] = klass
+      klass
+    rescue
+      raise "No such design system class: #{class_name}"
+    end
   end
 end
--- lib/buildout_design_system/engine.rb
+++ lib/buildout_design_system/engine.rb
@@ -3,5 +3,9 @@ require 'view_component'
 module BuildoutDesignSystem
   class Engine < ::Rails::Engine
     isolate_namespace BuildoutDesignSystem
+
+    config.to_prepare do
+      ::ApplicationController.helper BuildoutDesignSystem::ApplicationHelper
+    end
   end
 end
