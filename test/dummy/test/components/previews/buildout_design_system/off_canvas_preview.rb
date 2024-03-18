c39b4f5beaeea8d9fa2a4eeb98075eaa40f59a15
Joel <joel.lopez@buildout.com>
2024-03-18 16:14:26 -0700

    Making body more descriptive. Adding Param for Title.

--- test/dummy/test/components/previews/buildout_design_system/off_canvas_preview.rb
+++ test/dummy/test/components/previews/buildout_design_system/off_canvas_preview.rb
@@ -5,11 +5,13 @@ module BuildoutDesignSystem
     # Off Canvas / Side Drawer
     # -------------------------
     # Off Canvas or Side drawers provide a way to display temporary information or action that interrupts the user flow.
+    # @param title text "Title of the Off Canvas"
     # @param position select { choices: [~, start, end, top, bottom] } "Position of the Off Canvas"
     # @param class_name text " Additional Class names for the Off Canvas"
 
-    def default(position: "start", class_name: "")
+    def default(title: "Off Canvas Title", position: "start", class_name: "")
       render_with_template(locals: {
+        title: title,
         position: position,
         class_name: class_name
       })
