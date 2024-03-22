92149bded282854787ca114047b87027eb30ce12
tjacobs3 <tj@buildout.com>
2024-03-22 15:30:37 -0500

    Add popover and tooltip previews #14 #13

---
+++ test/dummy/test/components/previews/buildout_design_system/popover_preview.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module BuildoutDesignSystem
+  class PopoverPreview < ViewComponent::Preview
+    # @!group Popover
+
+    # Popover
+    # -------------------------
+    # Default bootstrap Popover
+    def default; end
+
+    # @!endgroup
+  end
+end
---
+++ test/dummy/test/components/previews/buildout_design_system/tooltip_preview.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module BuildoutDesignSystem
+  class TooltipPreview < ViewComponent::Preview
+    # @!group Tooltip
+
+    # Tooltip
+    # -------------------------
+    # Default bootstrap tooltip
+    def default; end
+
+    # @!endgroup
+  end
+end
