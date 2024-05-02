203066cd9c016c2aa91ee365decd92456ae3b7d9
nengler <englernathan@gmail.com>
2024-05-02 15:02:11 -0500

    added missing badge sizes

--- app/components/buildout_design_system/badge.rb
+++ app/components/buildout_design_system/badge.rb
@@ -3,24 +3,32 @@
 module BuildoutDesignSystem
   class Badge < ViewComponent::Base
     VARIANTS = {
-        primary: "primary",
-        secondary: "secondary",
-        success: "success",
-        danger: "danger",
-        warning: "warning",
-        info: "info",
-        neutral: "neutral",
-      }.freeze
+      primary: "primary",
+      secondary: "secondary",
+      success: "success",
+      danger: "danger",
+      warning: "warning",
+      info: "info",
+      neutral: "neutral",
+    }.freeze
 
-      DEFAULT_VARIANT = VARIANTS.fetch(:primary)
+    SIZES = {
+      default: "default",
+      sm: "sm",
+      xs: "xs",
+    }.freeze
 
-      def initialize(options = {}, **attrs)
-        super(**attrs)
-        @variant = VARIANTS.fetch(options[:variant]&.to_sym || :primary, DEFAULT_VARIANT)
-        @icon = options[:icon]
-        @class_name = options[:class_name]
-        @placement = options.fetch(:placement, "start")
-        @attrs = attrs
-      end
+    DEFAULT_VARIANT = VARIANTS.fetch(:primary)
+    DEFAULT_SIZE = SIZES.fetch(:default)
+
+    def initialize(options = {}, **attrs)
+      super(**attrs)
+      @variant = VARIANTS.fetch(options[:variant]&.to_sym || :primary, DEFAULT_VARIANT)
+      @size = SIZES.fetch(options[:size]&.to_sym || :default, DEFAULT_SIZE)
+      @icon = options[:icon]
+      @class_name = options[:class_name]
+      @placement = options.fetch(:placement, "start")
+      @attrs = attrs
+    end
   end
 end
