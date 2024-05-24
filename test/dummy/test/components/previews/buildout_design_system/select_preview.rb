f3d1e3552430600f75643007a9e3c3633b5decee
nengler <englernathan@gmail.com>
2024-05-24 14:53:58 -0500

    updated select styling

---
+++ test/dummy/test/components/previews/buildout_design_system/select_preview.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module BuildoutDesignSystem
+  class SelectPreview < ViewComponent::Preview
+
+    # Select
+    def default
+      render_with_template
+    end
+  end
+end
