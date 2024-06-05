cce501222704deb28d67d5e750a0e95afc1d0b8b
lukemeyer94 <luke.meyer@buildout.com>
2024-06-05 09:24:13 -0500

    add display limit and allow HTML tooltips on Avatar component

--- app/components/buildout_design_system/avatar.rb
+++ app/components/buildout_design_system/avatar.rb
@@ -8,10 +8,13 @@ module BuildoutDesignSystem
       lg: "lg"
     }.freeze
 
-    def initialize(images: [], size: "sm")
-
+    def initialize(images: [], size: "sm", display_limit: nil)
       @images = Array.wrap(images)
       @size_class = SIZES[size.to_sym] ? "avatar-#{size}" : ""
+      display_limit ||= @images.count
+
+      @visible_images = @images[0..(display_limit - 1)]
+      @hidden_images = @images[display_limit..]
     end
   end
 end
