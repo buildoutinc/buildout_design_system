267b7061240f967fe4c707736a64fcc62e8a349e
nengler <englernathan@gmail.com>
2024-06-11 10:27:50 -0500

    add select error styling

--- test/dummy/test/components/previews/buildout_design_system/select_preview.rb
+++ test/dummy/test/components/previews/buildout_design_system/select_preview.rb
@@ -4,8 +4,9 @@ module BuildoutDesignSystem
   class SelectPreview < ViewComponent::Preview
 
     # Select
-    def default
-      render_with_template
+    # @param is_invalid toggle
+    def default(is_invalid: false)
+      render_with_template(locals: { is_invalid: is_invalid })
     end
   end
 end
